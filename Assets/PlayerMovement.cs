488785db5ef485c196d7b230a2966e659793b641
Panu Niskakoski <46327100+panuniskakoski@users.noreply.github.com>
2020-05-29 19:02:51 +0300

    Iteration 1.

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,18 +7,47 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController2D controller;
 
     public float walkSpeed = 40f;
+    public Animator animator;
 
     float horizontalMove = 0f;
+    bool interaction = false;
 
     // Update is called once per frame
-    void Update() {
-
+    void Update()
+    {
         horizontalMove = Input.GetAxisRaw("Horizontal") * walkSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("Interact"))
+        {
+            interaction = true;
+        }
     }
 
-    void FixedUpdate()
-    {
+    
+    void FixedUpdate() {
         // Move the character
         controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+        interaction = false;
+
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //Debug.Log("Hit detected!");
+        //  && interaction == true
+
+        
+        if (other.tag == "Player")
+        {
+            Debug.Log("Hit detected!");
+            // other.GetComponent<Renderer>().enabled = false;
+        }
+        else
+        {
+            // other.GetComponent<Renderer>().enabled = true;
+        }
     }
 }
