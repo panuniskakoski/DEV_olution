459bdb48569c55308b2230fca2e5775a2926950c
Panu Niskakoski <46327100+panuniskakoski@users.noreply.github.com>
2020-05-29 16:48:19 +0300

    Alkusetit
    Eka talletus

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    public float walkSpeed = 40f;
+
+    float horizontalMove = 0f;
+
+    // Update is called once per frame
+    void Update() {
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * walkSpeed;
+    }
+
+    void FixedUpdate()
+    {
+        // Move the character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+    }
+}
---
+++ Assets/screen_size.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAdjust : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        int height = 2000;
+        Camera.main.scaledPixelHeight.Equals(height);
+
+        int width = 4000;
+        Camera.main.scaledPixelWidth.Equals(width);
+    }
+}
