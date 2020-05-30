62bdbbcaf98df4b9bc1bbfa1a674ed2ce231bfa7
Panu Niskakoski <46327100+panuniskakoski@users.noreply.github.com>
2020-05-30 13:35:22 +0300

    kolmas iteraatio
    Nyt toimii kaikki animaatiot oikein. Seuraavaksi stat system

---
+++ Assets/Appearance.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Appearance : MonoBehaviour
+{
+    public bool unlocked = false;
+
+    public void Start()
+    {
+        this.GetComponent<Renderer>().enabled = false;
+    }
+
+    public void Update()
+    {
+
+    }
+
+    public void ToggleUnlock()
+    {
+        unlocked = true;
+        this.GetComponent<Renderer>().enabled = true;
+    }
+}
